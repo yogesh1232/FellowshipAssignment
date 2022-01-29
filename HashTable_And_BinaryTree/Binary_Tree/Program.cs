a01026e5aca6ed63b34fd6d1faca843954213460
yogesh1232 <yogeshkusakiya77@gmail.com>
2022-01-29 18:06:31 +0530

    Assignment

---
+++ HashTable_And_BinaryTree/Binary_Tree/Program.cs
@@ -0,0 +1,28 @@
+﻿using Binary_Search_Tree;
+
+Console.WriteLine("\\nHello World!"); //[3,7,9,11,55,67]
+BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+binarySearch.Insert(30);
+binarySearch.Insert(70);
+binarySearch.Insert(22);
+binarySearch.Insert(40);
+binarySearch.Insert(60);
+binarySearch.Insert(95);
+binarySearch.Insert(11);
+binarySearch.Insert(65);
+binarySearch.Insert(3);
+binarySearch.Insert(16);
+binarySearch.Insert(63);
+binarySearch.Insert(67);
+binarySearch.Display();
+bool result = binarySearch.IfExists(63, binarySearch);
+Console.WriteLine(result);
+if (result == true)
+{
+    Console.WriteLine("Key found");
+}
+else
+{
+    Console.WriteLine("Key not found");
+}
+Console.ReadKey();
---
+++ HashTable_And_BinaryTree/HashTable_And_BinaryTree/Program.cs
@@ -0,0 +1,28 @@
+﻿using HashTable_And_BinaryTree;
+
+Console.WriteLine("Hash table demo"); //() []
+MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+hash.Add("0", "Paranoids");
+hash.Add("1", "are");
+hash.Add("2", "not");
+hash.Add("3", "paranoid");
+hash.Add("4", "because");
+hash.Add("5", "they");
+hash.Add("6", "are");
+hash.Add("7", "paranoid");
+hash.Add("8", "but");
+hash.Add("9", "because");
+hash.Add("10", "they");
+hash.Add("11", "keep");
+hash.Add("12", "putting");
+hash.Add("13", "themselves");
+hash.Add("14", "deliberately");
+hash.Add("15", "into ");
+hash.Add("16", "paranoid");
+hash.Add("17", "avoidable");
+hash.Add("18", "situations");
+//// remove word from hashtable.
+string hash17 = hash.Get("17");
+Console.WriteLine("17th index value:" + hash17);
+hash.Remove(hash17);
+Console.ReadKey();
