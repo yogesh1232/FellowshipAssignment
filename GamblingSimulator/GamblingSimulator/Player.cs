5ec0558da2a671e404aaa30651ca1be8704fb892
yogesh1232 <yogeshkusakiya77@gmail.com>
2022-01-31 18:45:17 +0530

    Assignment

--- GamblingSimulator/GamblingSimulator/Player.cs
+++ GamblingSimulator/GamblingSimulator/Player.cs
@@ -7,15 +7,15 @@ using System.Threading.Tasks;
 namespace GamblingSimulator
 {
     public class Player
-    { 
+    {
         public static void GameStatus()
         {
             int STAKE_PER_DAY = 100;
-            int BET_PER_GAME = 1;
+            int BET_PER_DAY = 1;
 
         }
         
-        }         
+            
 
     }
 }
